026f161bd680dedea6c6295b21dc2c7c1232750e
Ace The King <solcat0414@gmail.com>
2024-06-04 11:46:25 -0400

    Silence effect is now unclearable by milk. Added silence spell to also effect spell casting mobs, untested

--- src/main/java/net/acetheeldritchking/discerning_the_eldritch/effects/SilencePotionEffect.java
+++ src/main/java/net/acetheeldritchking/discerning_the_eldritch/effects/SilencePotionEffect.java
@@ -1,36 +1,20 @@
 package net.acetheeldritchking.discerning_the_eldritch.effects;
 
-import io.redspace.ironsspellbooks.api.magic.MagicData;
-import io.redspace.ironsspellbooks.api.util.Utils;
-import net.acetheeldritchking.discerning_the_eldritch.registeries.DTEPotionEffectRegistry;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SilencePotionEffect extends MobEffect {
     public SilencePotionEffect() {
         super(MobEffectCategory.HARMFUL, 3755355);
     }
 
-    /*@SubscribeEvent
-    public static void onLivingTickEvent(TickEvent.PlayerTickEvent event)
-    {
-        var entity = event.player;
-        var level = entity.level;
-        var playerMagicData = MagicData.getPlayerMagicData(entity);
-
-        if (entity instanceof ServerPlayer player)
-        {
-            if (!level.isClientSide)
-            {
-                if (player.hasEffect(DTEPotionEffectRegistry.SILENCE_POTION_EFFECT.get())
-                        && playerMagicData.isCasting())
-                {
-                    Utils.serverSideCancelCast(player, true);
-                }
-            }
-        }
-    }*/
+    @Override
+    public List<ItemStack> getCurativeItems() {
+        ArrayList<ItemStack> ret = new ArrayList<ItemStack>();
+        return ret;
+    }
 }
--- src/main/java/net/acetheeldritchking/discerning_the_eldritch/events/ServerEvents.java
+++ src/main/java/net/acetheeldritchking/discerning_the_eldritch/events/ServerEvents.java
@@ -3,6 +3,7 @@ package net.acetheeldritchking.discerning_the_eldritch.events;
 import io.redspace.ironsspellbooks.api.events.SpellPreCastEvent;
 import io.redspace.ironsspellbooks.api.magic.MagicData;
 import io.redspace.ironsspellbooks.api.util.Utils;
+import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
 import net.acetheeldritchking.discerning_the_eldritch.registeries.DTEPotionEffectRegistry;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
@@ -10,19 +11,22 @@ import net.minecraft.network.chat.TextColor;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber
 public class ServerEvents {
-    // Silence effect preventing spell casting
+    // Silence effect preventing spell casting on player
     @SubscribeEvent
-    public static void onLivingTickEvent(SpellPreCastEvent event) {
+    public static void onPlayerCastEvent(SpellPreCastEvent event) {
         var entity = event.getEntity();
-        if (entity instanceof ServerPlayer player && !player.level.isClientSide) {
-            boolean hasSilenceEffect = player.hasEffect(DTEPotionEffectRegistry.SILENCE_POTION_EFFECT.get());
-
-            if (hasSilenceEffect) {
+        boolean hasSilenceEffect = entity.hasEffect(DTEPotionEffectRegistry.SILENCE_POTION_EFFECT.get());
+        if (entity instanceof ServerPlayer player && !player.level.isClientSide)
+        {
+            if (hasSilenceEffect)
+            {
                 event.setCanceled(true);
                 // Effect Duration
                 int time = player.getEffect(DTEPotionEffectRegistry.SILENCE_POTION_EFFECT.get()).getDuration();
@@ -35,8 +39,24 @@ public class ServerEvents {
                         SoundEvents.FIRE_EXTINGUISH , SoundSource.PLAYERS , 0.5f , 1f);
             }
         }
-
     }
+
+    // Silence effect preventing spell casting on mobs
+    @SubscribeEvent
+    public static void onEntityCastEvent(LivingEvent.LivingTickEvent event)
+    {
+        var entity = event.getEntity();
+        boolean hasSilenceEffect = entity.hasEffect(DTEPotionEffectRegistry.SILENCE_POTION_EFFECT.get());
+
+        if (entity instanceof AbstractSpellCastingMob caster && !caster.level.isClientSide)
+        {
+            if (hasSilenceEffect)
+            {
+                caster.cancelCast();
+            }
+        }
+    }
+
     public static String convertTicksToTime(int ticks) {
         // Convert ticks to seconds
         int totalSeconds = ticks / 20;
