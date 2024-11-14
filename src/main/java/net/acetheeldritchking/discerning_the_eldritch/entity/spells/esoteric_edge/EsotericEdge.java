88c48c207f6f04486b334532ebb48257ec0b22ff
Ace The King <solcat0414@gmail.com>
2024-11-14 11:03:29 -0500

    Changelog:
    Eso can now hit multiple entities at the same time

--- src/main/java/net/acetheeldritchking/discerning_the_eldritch/entity/spells/esoteric_edge/EsotericEdge.java
+++ src/main/java/net/acetheeldritchking/discerning_the_eldritch/entity/spells/esoteric_edge/EsotericEdge.java
@@ -1,11 +1,13 @@
 package net.acetheeldritchking.discerning_the_eldritch.entity.spells.esoteric_edge;
 
 import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.capabilities.magic.MagicManager;
 import io.redspace.ironsspellbooks.damage.DamageSources;
 import io.redspace.ironsspellbooks.entity.mobs.AntiMagicSusceptible;
 import io.redspace.ironsspellbooks.entity.spells.AbstractMagicProjectile;
 import io.redspace.ironsspellbooks.entity.spells.AbstractShieldEntity;
 import io.redspace.ironsspellbooks.entity.spells.ShieldPart;
+import io.redspace.ironsspellbooks.util.ParticleHelper;
 import net.acetheeldritchking.discerning_the_eldritch.registries.DTEEntityRegistry;
 import net.acetheeldritchking.discerning_the_eldritch.registries.SpellRegistries;
 import net.minecraft.core.Holder;
@@ -15,22 +17,31 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class EsotericEdge extends AbstractMagicProjectile implements AntiMagicSusceptible {
+    private List<Entity> entities;
+
     public EsotericEdge(EntityType<? extends Projectile> entityType, Level level) {
         super(entityType, level);
 
+        entities = new ArrayList<>();
         this.setNoGravity(true);
     }
 
@@ -59,6 +70,17 @@ public class EsotericEdge extends AbstractMagicProjectile implements AntiMagicSu
 
     @Override
     public void tick() {
+        if (!level().isClientSide)
+        {
+            HitResult hitresult = ProjectileUtil.getHitResultOnMoveVector(this, this::canHitEntity);
+            if (hitresult.getType() == HitResult.Type.BLOCK) {
+                onHitBlock((BlockHitResult) hitresult);
+            }
+            for (Entity entity : level().getEntities(this, this.getBoundingBox()).stream().filter(target -> canHitEntity(target) && !entities.contains(target)).collect(Collectors.toSet())) {
+                damageEntity(entity);
+            }
+        }
+
         Vec3 deltaMovement = getDeltaMovement();
         double distance = deltaMovement.horizontalDistance();
 
@@ -70,6 +92,7 @@ public class EsotericEdge extends AbstractMagicProjectile implements AntiMagicSu
         setXRot((float) (Mth.atan2(y, distance) * (180 / Math.PI)));
         setXRot(lerpRotation(xRotO, getXRot()));
         setYRot(lerpRotation(yRotO, getYRot()));
+
         super.tick();
     }
 
@@ -93,37 +116,82 @@ public class EsotericEdge extends AbstractMagicProjectile implements AntiMagicSu
         this.damage = damage;
     }
 
-    @Override
+    /*@Override
     protected void onHitEntity(EntityHitResult pResult) {
         super.onHitEntity(pResult);
         var target = pResult.getEntity();
 
-        DamageSources.applyDamage(target, damage,
-                SpellRegistries.ESOTERIC_EDGE.get().getDamageSource(this, getOwner()));
+        if (!entities.contains(target))
+        {
+            DamageSources.applyDamage(target, damage,
+                    SpellRegistries.ESOTERIC_EDGE.get().getDamageSource(this, getOwner()));
 
-        // Kills shields & Do effects
-        if (target instanceof LivingEntity livingTarget)
-        {
-            livingTarget.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 60, 0));
-            if (livingTarget instanceof Player player)
+            // Kills shields & Do effects
+            if (target instanceof LivingEntity livingTarget)
             {
-                player.disableShield();
+                livingTarget.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 60, 0));
+                if (livingTarget instanceof Player player)
+                {
+                    player.disableShield();
+                }
+                if (DamageSources.applyDamage(livingTarget, damage, SpellRegistries.ESOTERIC_EDGE.get().getDamageSource(this, getOwner())))
+                {
+                    EnchantmentHelper.doPostAttackEffects((ServerLevel) this.level(), livingTarget, SpellRegistries.ESOTERIC_EDGE.get().getDamageSource(this, getOwner()));
+                }
             }
-            if (DamageSources.applyDamage(livingTarget, damage, SpellRegistries.ESOTERIC_EDGE.get().getDamageSource(this, getOwner())))
+            if (target instanceof ShieldPart || target instanceof AbstractShieldEntity)
             {
-                EnchantmentHelper.doPostAttackEffects((ServerLevel) this.level(), livingTarget, SpellRegistries.ESOTERIC_EDGE.get().getDamageSource(this, getOwner()));
+                target.kill();
             }
+
+            entities.add(target);
         }
-        if (target instanceof ShieldPart || target instanceof AbstractShieldEntity)
+    }*/
+
+    private void damageEntity(Entity entity)
+    {
+        if (!entities.contains(entity))
         {
-            target.discard();
+            DamageSources.applyDamage(entity, damage,
+                    SpellRegistries.ESOTERIC_EDGE.get().getDamageSource(this, getOwner()));
+
+            // Kills shields & Do effects
+            if (entity instanceof LivingEntity livingTarget)
+            {
+                livingTarget.addEffect(new MobEffectInstance(MobEffects.DARKNESS, 60, 0));
+                if (livingTarget instanceof Player player)
+                {
+                    player.disableShield();
+                }
+                if (DamageSources.applyDamage(livingTarget, damage, SpellRegistries.ESOTERIC_EDGE.get().getDamageSource(this, getOwner())))
+                {
+                    EnchantmentHelper.doPostAttackEffects((ServerLevel) this.level(), livingTarget, SpellRegistries.ESOTERIC_EDGE.get().getDamageSource(this, getOwner()));
+                }
+            }
+            if (entity instanceof ShieldPart || entity instanceof AbstractShieldEntity)
+            {
+                entity.kill();
+            }
+
+            entities.add(entity);
         }
     }
 
-    @Override
+    /*@Override
     protected void onHit(HitResult hitresult) {
         super.onHit(hitresult);
         this.discard();
+    }*/
+
+    @Override
+    protected void onHitBlock(BlockHitResult result) {
+        super.onHitBlock(result);
+        this.discard();
+    }
+
+    @Override
+    protected boolean canHitEntity(Entity pTarget) {
+        return pTarget != getOwner() && super.canHitEntity(pTarget);
     }
 
     @Override
